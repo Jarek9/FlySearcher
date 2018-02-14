7a9ada19d6c732cc5abfe04792547918efaad3f1
Jarek9 <jbabecki@wp.pl>
2018-02-14 19:33:53 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+			Gui gui = new Gui();
+	        gui.setVisible(true);
+	        gui.setLocationRelativeTo(null);
+	        gui.setSize(800, 530);
+	        gui.setResizable(false);
+	        gui.setTitle("FLIGHT SEARCHER");
+	        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
